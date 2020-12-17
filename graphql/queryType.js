5608fb0797b1d9558d831122083c7b209d534b03
Dinescu Alexandru-Catalin <alexdinescu888@gmail.com>
2020-12-17 23:59:35 +0200

    Defined queries

--- graphql/queryType.js
+++ graphql/queryType.js
@@ -1,13 +1,42 @@
-const { GraphQLObjectType, GraphQLNonNull, GraphQLInt, GraphQLString } = require('graphql');
+const { GraphQLObjectType, GraphQLInt } = require('graphql');
+const types = require('./types')
+const models = require('../models');
 
 const queryType = new GraphQLObjectType({
-    name: 'HelloWorld',
+    name: 'RootQuery',
     fields: () => ({
-        message: {
-            type: GraphQLString,
-            resolve: () => 'Hello World'
-        }
-    })
-  });
+        user: {
+            type: types.userType,
+            description: "Return a specific user",
+            args: {
+                id: { type: GraphQLInt }
+            },
+            resolve: async (parent,args) => {
+                return await models.User.findByPk(args.id);
+            }
+        },
+        review: {
+            type: types.reviewType,
+            description: "Return a specific review",
+            args: {
+                id: { type: GraphQLInt }
+            },
+            resolve: async (parent,args) => {
+                return await models.Review.findByPk(args.id);
+            }
+        },
+        product: {
+            type: types.productType,
+            description: "Return a specific product",
+            args: {
+                id: { type: GraphQLInt }
+            },
+            resolve: async (parent,args) => {
+                return await models.Product.findByPk(args.id);
+            }
+        },
+
+	})
+});
 
   module.exports = queryType; 
---
+++ graphql/types.js
@@ -0,0 +1,71 @@
+const { GraphQLObjectType, 
+    GraphQLInt, 
+    GraphQLString, 
+    GraphQLNonNull, 
+    GraphQLList } = require('graphql');
+const models = require('../models');
+
+const reviewType = new GraphQLObjectType({
+    name: "Review",
+    description: "This represents a review",
+    fields: () => {
+        return ({
+            id: { type: GraphQLNonNull(GraphQLInt)},
+            userId: { type: GraphQLNonNull(GraphQLInt)},
+            text: { type: GraphQLNonNull(GraphQLString)},
+            rating: { type: GraphQLNonNull(GraphQLInt)},
+            createdAt: { type: GraphQLNonNull(GraphQLString)},
+            updatedAt: { type: GraphQLNonNull(GraphQLString)},
+            user: {
+                type: userType,
+                resolve: async (parent) => {
+                   await parent.getUser();
+                }
+            },
+        });
+    }
+})
+
+const userType = new GraphQLObjectType({
+    name: "User",
+    description: "This represents a customer",
+    fields: () => ({
+        id: { type: GraphQLNonNull(GraphQLInt)},
+        username: { type: GraphQLNonNull(GraphQLString)},
+        email: { type: GraphQLNonNull(GraphQLString)},
+        profilePicture: { type: GraphQLNonNull(GraphQLString)},
+        reviews: {
+            type: GraphQLList(reviewType),
+            resolve: async (parent) => {
+                return await parent.getReviews();
+            }
+        },
+        createdAt: { type: GraphQLNonNull(GraphQLString)},
+        updatedAt: { type: GraphQLNonNull(GraphQLString)}
+    })
+});
+
+const productType = new GraphQLObjectType({
+    name: "Product",
+    description: "This represents a product",
+    fields: () => ({
+        id: { type: GraphQLNonNull(GraphQLInt)},
+        name: { type: GraphQLNonNull(GraphQLString)},
+        rating: { type: GraphQLNonNull(GraphQLInt)},
+        image: { type: GraphQLNonNull(GraphQLString)},
+        description: { type: GraphQLNonNull(GraphQLString)},
+        createdAt: { type: GraphQLNonNull(GraphQLString)},
+        updatedAt: { type: GraphQLNonNull(GraphQLString)}
+    })
+});
+
+
+
+
+const types = {
+    userType,
+    reviewType,
+    productType
+}
+
+module.exports = types; 
